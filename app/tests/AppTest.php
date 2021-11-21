af67954b6729a261adde2bcb74730876ff50d92b
przemekdab1993 <przemekdab1993@gmail.com>
2021-11-21 13:54:07 +0100

    init code

---
+++ app/tests/AppTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AppTest extends KernelTestCase
+{
+    /**
+     * ddwdw
+     */
+    public function testAppWorks()
+    {
+        $this->assertTrue(true);
+    }
+
+}
