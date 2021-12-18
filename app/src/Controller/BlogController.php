e346071b658abf67886a30e10cba8bf565d2296a
przemekdab1993 <przemekdab1993@gmail.com>
2021-12-18 20:07:08 +0100

    app/make command

--- app/src/Controller/BlogController.php
+++ app/src/Controller/BlogController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
+use function PHPUnit\Framework\throwException;
 
 class BlogController extends AbstractController
 {
