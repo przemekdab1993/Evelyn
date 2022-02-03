50bf265a95806e671b39a69ef73aa55b260029f8
przemekdab1993 <przemekdab1993@gmail.com>
2022-02-03 09:11:20 +0100

    timestemps in entity

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
--- app/src/Entity/DocRating.php
+++ app/src/Entity/DocRating.php
@@ -4,12 +4,16 @@ namespace App\Entity;
 
 use App\Repository\DocRatingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableDocument;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass=DocRatingRepository::class)
  */
 class DocRating
 {
+    use TimestampableEntity;
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
