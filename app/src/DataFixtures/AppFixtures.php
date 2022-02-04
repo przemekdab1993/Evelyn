14aae8214401d1553106b20eca9e40967d3c48de
przemekdab1993 <przemekdab1993@gmail.com>
2022-02-04 21:04:58 +0100

    fixture factory practis

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Doc;
 use App\Entity\DocRating;
 use App\Factory\DocFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,5 +14,6 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         DocFactory::new()->createMany(10);
+        UserFactory::new()->createMany(5);
     }
 }
