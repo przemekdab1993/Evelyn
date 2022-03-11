4f9d49d4bb142b9a76a2995430c97724d952bdc0
przemekdab1993 <przemekdab1993@gmail.com>
2022-03-11 07:13:27 +0100

    odblokowanie w ustawieniach komunikatu o nieprawidłowym loginie, dodanie do widoku powiadomień o nieprawidłowej autoryzacji

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('security/login.html.twig', [
-
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
 }
