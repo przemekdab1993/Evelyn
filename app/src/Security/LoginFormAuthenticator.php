9aa2eb267381082bd25941bc781927ed673b8227
przemekdab1993 <przemekdab1993@gmail.com>
2022-03-15 08:32:25 +0100

    zakomentowanie nadmiarowego kodu, który został zbędnie napisany z powodu nie wybrania autentykacji za pomocą hasła

--- app/src/Security/LoginFormAuthenticator.php
+++ app/src/Security/LoginFormAuthenticator.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -21,9 +22,12 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator //AbstractAuthenticator implements AuthenticationEntryPointInterface
 {
+    use TargetPathTrait;
+
     /**
      * @var UserRepository
      */
@@ -40,10 +44,10 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
         $this->router = $router;
     }
 
-    public function supports(Request $request): ?bool
-    {
-        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
-    }
+//    public function supports(Request $request): ?bool
+//    {
+//        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
+//    }
 
     public function authenticate(Request $request): PassportInterface
     {
@@ -73,24 +77,34 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
     {
         $request->getSession()->set('UserLogin', true);
 
+        if ($target = $this->getTargetPath($request->getSession(), $firewallName)) {
+
+            return new RedirectResponse($target);
+        }
+
         return new RedirectResponse(
             $this->router->generate('index')
         );
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+//    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+//    {
+//        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+//
+//        return new RedirectResponse(
+//            $this->router->generate('login')
+//        );
+//    }
 
-        return new RedirectResponse(
-            $this->router->generate('login')
-        );
-    }
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        return new RedirectResponse(
+//            $this->router->generate('login')
+//        );
+//    }
 
-    public function start(Request $request, AuthenticationException $authException = null): Response
+    protected function getLoginUrl(Request $request): string
     {
-        return new RedirectResponse(
-            $this->router->generate('login')
-        );
+        return $this->router->generate('login');
     }
 }
