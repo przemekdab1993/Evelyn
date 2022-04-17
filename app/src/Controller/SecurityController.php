472ba0251f854cb95e83086d38bafafd4306c1b1
przemekdab1993 <przemekdab1993@gmail.com>
2022-04-17 09:06:14 +0200

    generowanie QR code, wyświetlanie go użytkownikom

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Util\Exception;
 use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\QrCode\QrCodeGenerator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,7 +31,7 @@ class SecurityController extends BaseController
 
 
     #[Route('/authenticate/2fa/enable', name: '2fa_enable')]
-    #[IsGranted('ROLE_USER')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function enable2fa(TotpAuthenticatorInterface $totpAuthenticator, EntityManagerInterface $entityManager)
     {
         $user = $this->getUser();
@@ -41,7 +42,16 @@ class SecurityController extends BaseController
             $entityManager->flush();
         }
 
-        dd($user);
+        return $this->render('security/enable_2fa.html.twig');
+    }
 
+    #[Route('/authenticate/2fa/qr-code', name: 'qr_code')]
+    public function displayGoogleAuthenticatorQrCode(QrCodeGenerator $qrCodeGenerator)
+    {
+        // $qrCode is provided by the endroid/qr-code library. See the docs how to customize the look of the QR code:
+        // https://github.com/endroid/qr-code
+        $qrCode = $qrCodeGenerator->getTotpQrCode($this->getUser());
+
+        return new Response($qrCode->writeString(), 200, ['Content-Type' => 'image/png']);
     }
 }
