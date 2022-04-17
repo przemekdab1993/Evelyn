038dfe6db352aee9894cddfe5f17bde913e28d07
przemekdab1993 <przemekdab1993@gmail.com>
2022-04-17 07:11:09 +0200

    2fa - generowanie kodu dla użytkownika, przycisk w menu usera

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Util\Exception;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class SecurityController extends BaseController
 {
     #[Route('/login', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -23,5 +26,22 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('logout() should never be reached.');
+    }
+
+
+    #[Route('/authenticate/2fa/enable', name: '2fa_enable')]
+    #[IsGranted('ROLE_USER')]
+    public function enable2fa(TotpAuthenticatorInterface $totpAuthenticator, EntityManagerInterface $entityManager)
+    {
+        $user = $this->getUser();
+
+        if (!$user->isTotpAuthenticationEnabled()) {
+            $user->setTotpSecret($totpAuthenticator->generateSecret());
+
+            $entityManager->flush();
+        }
+
+        dd($user);
+
     }
 }
